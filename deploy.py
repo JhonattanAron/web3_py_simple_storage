fbfc75f3087060a8bb28cbee53b57325c8fd6b6b
AronLaptop <aron.cachago@gmail.com>
2023-12-01 11:38:17 -0500

    1.0.4

--- deploy.py
+++ deploy.py
@@ -1,17 +1,16 @@
-from solcx import compile_standard , install_solc
+from solcx import compile_standard, install_solc
 import json
 import os
 from py_dotenv import read_dotenv
-from web3 import Web3 
+from web3 import Web3
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 read_dotenv(dotenv_path)
 
-with open('./contracts/Simple_Storage.sol' , "r") as file:
+with open('./contracts/Simple_Storage.sol', "r") as file:
     simple_storage_file = file.read()
 
 # Instalamos Solidity Version
-
 install_solc("0.6.0")
 
 # Compilar
@@ -34,53 +33,63 @@ compile_sol = compile_standard({
             }
         }
     }
-}, 
-solc_version="0.6.0"
-)
-
+}, solc_version="0.6.0")
 
 with open("./json/compile_sol_json.json", "w") as file:
     json.dump(compile_sol, file)
 
-
-bytecode = compile_sol["contracts"]["Simple_Storage.sol"]["SimpleStorage"]["evm"][
-    "bytecode"
-]["object"]
-
+bytecode = compile_sol["contracts"]["Simple_Storage.sol"]["SimpleStorage"]["evm"]["bytecode"]["object"]
 
 abi = json.loads(compile_sol["contracts"]["Simple_Storage.sol"]["SimpleStorage"]["metadata"])["output"]["abi"]
 
-# Conexion a garnache para hacer el despliege
-
-w3 = Web3(Web3.HTTPProvider("HTTP://172.27.32.1:7545"))
+# Conexion a Ganache para hacer el despliegue
+w3 = Web3(Web3.HTTPProvider(os.getenv("RPC_SERVER")))
 
 chain_id = 1337
 my_address = os.getenv("MY_ADDRESS")
-private_key =  os.getenv("PRIVATE_KEY")
+private_key = os.getenv("PRIVATE_KEY")
 
-SimpkeStorage = w3.eth.contract(abi = abi, bytecode = bytecode);
-
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
 
 # Nonce
 nonce = w3.eth.get_transaction_count(my_address)
 
-# Construir la transaccion
-transaction = SimpkeStorage.constructor().build_transaction({
-    "chainId":chain_id,
-    "gasPrice":w3.eth.gas_price,
-    "from":my_address,
-    "nonce":nonce
+# Construir la transacción
+transaction = SimpleStorage.constructor().build_transaction({
+    "chainId": chain_id,
+    "gasPrice": w3.eth.gas_price,
+    "from": my_address,
+    "nonce": nonce
 })
 
-#firmar la transaccion
+# Firmar la transacción
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
 
-signed_txn = w3.eth.account.sign_transaction(transaction , private_key=private_key )
-
-
-#Enviar la transaccion
+# Enviar la transacción
 tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
 print("Waiting for transaction...")
 
 tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-print(tx_receipt.contractAddress  )
 
+# Trabajar con contratos desplegados
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+
+# CALL -> SIMULA UN LLAMADO PARA OBTENER UN VALOR
+# Transact
+print(simple_storage.functions.retrieve().call())
+
+store_transaction = simple_storage.functions.store(15).build_transaction({
+    "chainId": chain_id,
+    "gasPrice": w3.eth.gas_price,
+    "from": my_address,
+    "nonce": nonce + 1  # Incrementar el nonce para la nueva transacción
+})
+
+print("Modificando Valor")
+
+signed_transaction = w3.eth.account.sign_transaction(store_transaction, private_key=private_key)
+
+t_hash = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(t_hash)
+
+print(simple_storage.functions.retrieve().call())
